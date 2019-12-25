005cc550f1b3cdaea96f777c5fa6acbf262502b9
Ken Brooks <brooks.ken@gmail.com>
2019-12-25 16:47:29 -0500

    Add time calculation

--- app/asana/index.js
+++ app/asana/index.js
@@ -1,92 +1,95 @@
-function Db (text) {
-    const $xml = $($.parseXML(text))
+function Db (json) {
+    json = json.sort((a, b) => a.seq - b.seq)
     return {
-        $xml,
         getAsanas() {
-            return $xml.find("asanas > asana").toArray().map(x => ({
-                id: $(x).attr("id"),
-                name: $(x).attr("name"),
-                steps: $(x).find("step").toArray().map(y => ({
-                    count: $(y).attr("count"),
-                    breaths: $(y).attr("breaths"),
-                    text: $(y).text(),
-                }))
-            }))
-        }
-    }
-}
-
-function TimeDisplay () {
-    let seconds = 0
-    let $el = null
-    let self = null
-    return self = {
-        update(s) {
-            seconds = s
-            $el.replaceWith(self.render())
-        },
-        render() {
-            const time = new Date(seconds * 1000)
-                .toISOString()
-                .substr(11, 8)
-            return $el = $("<span>").html(time)
+            return json
         }
     }
 }
 
 function AsanaSelector (db) {
-    let options = $("<select>")
-        .prop("id", "options")
-        .prop("multiple", "multiple")
-        .css("width", "40%")
-        .append(db.getAsanas().map(asana => 
-            $("<option>")
-                .html(asana.name)))
-    let selected = $("<select>")
-        .prop("id", "selected")
-        .prop("multiple", "multiple")
-        .css("width", "40%")
-    return {
-        select() {
-            let toMove = options.find("option").filter(function () {
-                return this.selected
-            })
-            options.remove(toMove)
-            selected.append(toMove)
-        },
-        deselect() {
-            let toMove = selected.find("option").filter(function () {
-                return this.selected
-            })
-            selected.remove(toMove)
-            options.append(toMove)
+
+    let choices = 
+        $("<select>")
+            .prop("multiple", "multiple")
+            .css("width", "40%")
+            .append(db.getAsanas().map(asana => 
+                $("<option>")
+                    .data("asana", asana)
+                    .html(asana.name)))
+    
+    let chosen = 
+        $("<select>")
+            .prop("multiple", "multiple")
+            .css("width", "40%")
+
+    const hooks = []
+
+    function getChosen() {
+        return chosen
+            .find("option")
+            .toArray()
+            .map(x => $(x).data("asana"))
+    }
+
+    function moveSelected(a, b) {
+        a.find("option").filter(function () {
+            return this.selected
+        }).appendTo(b)
+        b.find("option")
+            .sort((x, y) => $(x).data("asana").seq - $(y).data("asana").seq)
+            .appendTo(b)
+        hooks.forEach(fn =>
+            fn(getChosen()))
+    }
+
+    const self = {
+        getChosen: getChosen,
+        onUpdate(fn) {
+            hooks.push(fn)
         },
         render() {
-            return $("<div>").append(options, selected)
+            return $("<div>").append(
+                $("<div>").append(
+                    choices, 
+                    chosen),
+                $("<div>").append(
+                    $("<button>")
+                        .html("<-")
+                        .on("click", () => moveSelected(chosen, choices)),
+                    $("<button>")
+                        .html("->")
+                        .on("click", () => moveSelected(choices, chosen))))
         }
     }
+
+    return self
 }
 
-fetch("/app/asana/data.xml")
-    .then(resp => resp.text())
-    .then(text => Db(text))
-    .then(db => 
+function Controls() {
 
-        $(document).ready(() => {
+    const $cycle = $("<input>")
+        .prop("type", "number")
+        .prop("id", "secPerBreath")
+        .prop("value", 6)
+        .on("input", () => hooks.forEach(fn => fn(getCycle())))
 
-            console.log(db.getAsanas())
+    function getCycle() {
+        return +$cycle.val()
+    }
 
-            let timeDisplay = TimeDisplay()
-            let asanaSelector = AsanaSelector(db)
+    const hooks = []
 
-            $(document.body).append(
-                $("<h1>").append("Ashtanga Series Builder"),
+    return {
+        onUpdate(fn) {
+            hooks.push(fn)
+        },
+        getCycle: getCycle,
+        render() {
+            return $("<div>").append(
                 $("<div>").append(
                     "Seconds per breath cycle: ",
-                    $("<input>")
-                        .prop("type", "number")
-                        .prop("id", "secPerBreath")
-                        .prop("value", 6)),
+                    $cycle),
                 $("<div>").append(
                     "Volume: ",
                     $("<input>")
@@ -108,19 +111,70 @@ fetch("/app/asana/data.xml")
                         .on("click", () => console.log("Resume")),
                     $("<button>")
                         .html("Reset")
-                        .on("click", () => console.log("Reset")),),
+                        .on("click", () => console.log("Reset"))))
+        }
+    }
+}
+
+function Status (controls, asanaSelector) {
+
+    let currentAsana = null
+
+    function formatTime (t) {
+        return new Date(t * 1000)
+            .toISOString()
+            .substr(11, 8)
+    }
+
+    const $time = $("<span>")
+        .text(formatTime(0))
+
+    const self = {
+        render() {
+            return $("<div>").append(
                 $("<div>").append(
-                    asanaSelector.render(),
-                    $("<div>").append(
-                        $("<button>")
-                            .html("<-")
-                            .on("click", () => asanaSelector.deselect()),
-                        $("<button>")
-                            .html("->")
-                            .on("click", () => asanaSelector.select()))),
+                    $("<span>")
+                        .text("Expected time: "),
+                    $time),
                 $("<div>").append(
-                    "Expected time: ",
-                    timeDisplay.render()),
-                $("<div>").append("Current asana: None"))
+                    $("<span>")
+                        .text("Current asana: "),
+                    $("<span>")
+                        .text(currentAsana ? currentAsana.name : "None")))
+        }
+    }
+
+    function updateTime(cycle, chosen) { 
+        $time.text(formatTime(cycle *
+            chosen.reduce((a0, asana) => 
+                a0 + asana.steps.reduce((a1, step) => 
+                    a1 + step.breaths, 0), 0)))
+    }
+
+    asanaSelector.onUpdate(chosen => 
+        updateTime(controls.getCycle(), chosen))
+
+    controls.onUpdate(cycle => 
+        updateTime(cycle, asanaSelector.getChosen()))
+
+    return self
+}
+
+fetch("/app/asana/data.json")
+    .then(resp => resp.json())
+    .then(json => Db(json))
+    .then(db => 
+
+        $(document).ready(() => {
+
+            let controls = Controls()
+            let asanaSelector = AsanaSelector(db)
+            let status = Status(controls, asanaSelector)
+
+            $(document.body).append(
+                $("<h1>").append("Ashtanga Series Builder"),
+                controls.render(),
+                asanaSelector.render(),
+                status.render())
 
     }))
