6038ea7a4996cfdf1a69c9d6c044ccc48aeac431
Ken Brooks <brooks.ken@gmail.com>
2019-12-26 17:36:21 -0500

    WIP: add player logic

--- app/asana/index.js
+++ app/asana/index.js
@@ -1,19 +1,18 @@
 fetch("/app/asana/data.json")
     .then(resp => resp.json())
-    .then(json => Db(json))
-    .then(db =>
+    .then(json => {
+ 
+        const db = Db(json)
+        const asanaSelector = AsanaSelector(db)
+        const player = Player(asanaSelector)
+        const controls = Controls(player)
+        const status = Status(player)
 
-        $(document).ready(() => {
-
-            const controls = Controls()
-            const asanaSelector = AsanaSelector(db)
-            const status = Status(controls, asanaSelector)
-
+        $(document).ready(() =>
             $(document.body).append(
                 $("<h1>").append("Ashtanga Series Builder"),
                 controls.$html,
                 asanaSelector.$html,
-                status.$html
-            )
+                status.$html))
 
-    }))
+    })
