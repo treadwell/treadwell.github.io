907c358907ae017e9fcfa035afdefb5da61b9062
Ken Brooks <brooks.ken@gmail.com>
2020-01-03 15:14:56 -0500

    Add logging of selected asanas in instructions.js

--- app/asana/instructions.js
+++ app/asana/instructions.js
@@ -1,8 +1,15 @@
 function Instructions(asanaSelector) {
+    
     const $html = $("<div>").append(
         $("<div>").append(
-            $("<h2>").append("Instructions"),
-            asanaSelector.$html))
+            $("<h2>").append("Instructions")))
+
+    function showChosen() {
+        asanas = asanaSelector.getChosen()
+        console.log(asanas)
+    }
+
+    asanaSelector.on("change-chosen", showChosen)
 
     return $html
 }
