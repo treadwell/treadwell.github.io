2860b662fbdfa41745e6396cb42e0e55de55862c
Ken Brooks <brooks.ken@gmail.com>
2020-02-01 15:19:51 -0500

    Refactor asana-selector

--- app/asana/asana-selector.js
+++ app/asana/asana-selector.js
@@ -1,44 +1,22 @@
 function AsanaSelector (db) {
 
     const $presets =
-        $("<select>")
-            .prop("multiple", "multiple")
-            .css("width", "25%")
-            .css("height", "2in")
-            .css("user-scalable", "no")
+        mkSelect()
             .append(db.presets.map(preset =>
-                $("<option>")
-                    .data("preset", preset)
-                    .html(preset.name)))
+                mkOption("preset", preset, preset.name)))
     
     const $groups =
-        $("<select>")
-            .prop("multiple", "multiple")
-            .css("width", "25%")
-            .css("height", "2in")
-            .css("user-scalable", "no")
+        mkSelect()
             .append(db.groups.map(group =>
-                $("<option>")
-                    .data("group", group)
-                    .html(group.name)))
+                mkOption("group", group, group.name)))
 
     const $choices =
-        $("<select>")
-            .prop("multiple", "multiple")
-            .css("width", "25%")
-            .css("height", "2in")
-            .css("user-scalable", "no")
+        mkSelect()
             .append(db.asanas.map(asana =>
-                $("<option>")
-                    .data("asana", asana)
-                    .html(asana.name)))
+                mkOption("asana", asana, asana.name)))
 
     const $chosen =
-        $("<select>")
-            .prop("multiple", "multiple")
-            .css("width", "25%")
-            .css("height", "2in")
-            .css("user-scalable", "no")
+        mkSelect()
 
     const $html =
         $("<div>").append(
@@ -60,11 +38,10 @@ function AsanaSelector (db) {
                     .on("click", () => removePreset()),
                 $("<button>")
                     .html("Add Group to Playlist")
-                    .on("click", () => addGroup($groups, $chosen)),
+                    .on("click", () => addGroup($groups, $chosen, "group")),
                 $("<button>")
                     .html("Add Posture to Playlist")
                     .on("click", () => moveSelected($choices, $chosen)),
-
                 $("<button>")
                     .html("Remove from Playlist")
                     .on("click", () => removeSelected($chosen))))
@@ -76,57 +53,40 @@ function AsanaSelector (db) {
         }
         db.addPreset(p)
         $presets.append(
-            $("<option>")
-                .data("preset", p)
-                .html(p.name))
+            mkOption("preset", p, p.name))
     }
 
-    function addGroup (groups, chosen, prop = "group") {
+    function addGroup (groups, chosen, dataAttributeName) {
         
         // extract the id's from selected groups
-        ids = []
+        const ids = []
 
         groups.find("option")
             .filter(function () {
                 return this.selected  // gets selected options
             })
             .toArray()
-            .map(x => $(x).data(prop).series
+            .map(x => $(x).data(dataAttributeName).series
                 .map(x => ids.push(x)))  // put ids in an array
   
-        // turn into the actual asana
-        function find_asana(id, asanas) {
-            return asanas.find(a => a.id === id)
-        }
+        const selected_asanas = ids.map(id => db.asanas.find(a => a.id == id))
 
-        selected_asanas = ids.map(id => find_asana(id, db.asanas))
-        // DISCUSS: selected_asanas = db.asanas.filter(x => ids.includes(x.id))
-        
         // rebuild the options
         chosen.append(selected_asanas.map(asana =>
-            $("<option>")
-                .data("asana", asana)
-                .html(asana.name)))
+            mkOption("asana", asana, asana.name)))
 
         $html.trigger("change-chosen")
     }
 
     function removePreset () {
-
-        function findObject(name, objects) {
-            return objects.find(a => a.name === name)
-        }
-
-        const removalOptions = $presets.find("option")  // gets selected options
+        $presets.find("option")  // gets selected options
             .filter(function () {
                 return this.selected 
+            })
+            .each(function () {
+                db.removePreset($(this).data("preset"))
             })  
-
-        removalOptions  // Turn into object and remove from local storage
-            .toArray()
-            .map(x => db.removePreset(findObject(x.innerText, db.presets)))
-        
-        removalOptions.remove()  // remove the options from the select window
+            .remove()
     }
 
     function removeSelected (a) {
@@ -153,6 +113,20 @@ function AsanaSelector (db) {
             .find("option")
             .toArray()
             .map(x => $(x).data("asana"))
+    }
+
+    function mkOption (dataAttributeName, dataAttributeValue, html) {
+        return $("<option>")
+            .data(dataAttributeName, dataAttributeValue)
+            .html(html)
+    }
+
+    function mkSelect () {
+        return $("<select>")
+            .prop("multiple", "multiple")
+            .css("width", "25%")
+            .css("height", "2in")
+            .css("user-scalable", "no")
     }
 
     return $html
