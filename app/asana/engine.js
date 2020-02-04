a5e84a5e03ee9902e126db29e1fc12c3e81aea93
Ken Brooks <brooks.ken@gmail.com>
2020-02-04 10:59:57 -0500

    Block in API functions

--- app/asana/engine.js
+++ app/asana/engine.js
@@ -1,4 +1,37 @@
 function Engine (callback) {
+
+    function play () {
+        console.log("play not implemented")
+    }
+    function pause () {
+        console.log("pause not implemented")
+    }
+    function reset () {
+        console.log("reset not implemented")
+    }
+    function rewind () {
+        console.log("rewind not implemented")
+    }
+    function enqueue () {
+        console.log("enqueue not implemented")
+    }
+    function dequeue () {
+        console.log("dequeue not implemented")
+    }
+    function savePlaylist () {
+        console.log("savePlaylist not implemented")
+    }
+    function deletePlaylist () {
+        console.log("deletePlaylist not implemented")
+    }
+
+    const currentAsana = null
+    const currentStep = null
+    const enqueuedAsanas = []
+    const totalTime = null
+    const remainingTime = null
+    const playlists = []
+
     Promise.all([
     
         fetch("/app/asana/data.json")
@@ -10,13 +43,17 @@ function Engine (callback) {
         fetch("/app/asana/groups.json")
             .then(resp => resp.json())
     
-    ]).then(([asanas, numbers, groups]) => callback(
-        {"asanas": asanas,
-    "numbers": numbers,
-    "groups" : groups
-}))
-    
-
-
-
+    ]).then(([asanas, numbers, groups]) => callback({
+        "asanas": asanas,
+        "numbers": numbers,
+        "groups" : groups,
+        play,
+        pause,
+        reset,
+        rewind,
+        enqueue,
+        dequeue,
+        savePlaylist,
+        deletePlaylist
+        }))
 }
