779952439fb7eb47c3b0fecdca989fcedba1d4ff
Ken Brooks <brooks.ken@gmail.com>
2020-02-05 09:43:16 -0500

    Add enqueue()

--- app/asana/engine.js
+++ app/asana/engine.js
@@ -1,36 +1,62 @@
 function Engine (callback) {
 
-    function play () {
-        console.log("play not implemented")
-    }
-    function pause () {
-        console.log("pause not implemented")
-    }
-    function reset () {
-        console.log("reset not implemented")
-    }
-    function rewind () {
-        console.log("rewind not implemented")
-    }
-    function enqueue () {
-        console.log("enqueue not implemented")
-    }
-    function dequeue () {
-        console.log("dequeue not implemented")
-    }
-    function savePlaylist () {
-        console.log("savePlaylist not implemented")
-    }
-    function deletePlaylist () {
-        console.log("deletePlaylist not implemented")
-    }
-
+    const asanas = []
     const currentAsana = null
     const currentStep = null
     const enqueuedAsanas = []
     const totalTime = null
     const remainingTime = null
     const playlists = []
+
+    function play () {
+        console.log("play not implemented")
+    }
+
+    function pause () {
+        console.log("pause not implemented")
+    }
+
+    function reset () {
+        console.log("reset not implemented")
+    }
+
+    function rewind () {
+        console.log("rewind not implemented")
+    }
+
+    function enqueue (id, asanas) {
+        // add individual asana or playlist / group to enqueuedAsanas
+        // input: id, (group, stored playlist or asana)
+        // output: enqueuedAsanas
+        // execute via:  "window.engine.enqueue("suryA", window.engine.asanas)" in console
+        
+        // if id in asanas:  add it
+        enqueuedAsanas.push(asanas.find(a => a.id == id))
+        
+        // if id in groups:  add them
+
+        // if name in stored playlists  add them
+
+        console.log(id, asana, enqueuedAsanas)
+ 
+        return enqueuedAsanas
+    }
+
+    function dequeue () {
+        // remove individual asana from enqueuedAsanas
+        // input: single id
+        // output: enqueuedAsanas
+        // if id in asanas:
+        console.log("dequeue not implemented")
+    }
+
+    function savePlaylist () {
+        console.log("savePlaylist not implemented")
+    }
+
+    function deletePlaylist () {
+        console.log("deletePlaylist not implemented")
+    }
 
     Promise.all([
     
