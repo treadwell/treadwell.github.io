0f92ce1251450451cf39c1425b0e0e4937928be3
treadwell <brooks.ken@gmail.com>
2020-02-23 17:36:49 -0500

    Implement library and now playing pages

--- app/asana/v2/src/ui.js
+++ app/asana/v2/src/ui.js
@@ -1,3 +1,39 @@
 function Ui (engine) {
-    console.log("starting UI")
+
+    const nowPlaying = NowPlaying(engine, () => view("library"))
+    const library = Library(engine, () => view("now-playing"))
+
+    const $view = $("<div>")
+    const $app = $("<div>").attr("id", "app").append($view)
+    
+    function view (v) {
+        $view.children().detach()
+        $view.append({
+            "now-playing": nowPlaying,
+            "library": library
+        }[v])
+    }
+
+    $(document.body).append($app)
+
+    view("now-playing")
+
+}
+
+function NowPlaying (engine, toLibrary) {
+
+    const $nowPlaying = $("<div>")
+        .text("Now Playing")
+        .on("click", toLibrary)
+
+    return () => $nowPlaying
+}
+
+function Library (engine, toNowPlaying) {
+
+    const $library = $("<div>")
+        .text("Library")
+        .on("click", toNowPlaying)
+
+    return () => $library
 }
